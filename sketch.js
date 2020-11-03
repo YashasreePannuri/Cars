ca3e63578f01b76f0b0ada9fffa55704b85fd91c
YashasreePannuri <73391354+YashasreePannuri@users.noreply.github.com>
2020-11-03 16:03:13 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var wall,car;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(50,90);
+  weight=random(400,1500);
+  car=createSprite(50, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
